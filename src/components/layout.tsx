766f23a42b4dc0185d051ec696dd2dddf9b3df94
SKP <skp.frl@gmail.com>
2020-05-27 17:42:10 +0530

    feat(themes): add ThemeProvider to layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import "typeface-rubik";
+import lightTheme from "@/themes/light-theme";
 
 const GlobalStyles = createGlobalStyle`
   *,
@@ -15,16 +16,19 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-size: 10px;
     font-family: "Rubik", sans-serif;
+    color: ${props => props.theme.colors.darkBlue};
+    position: relative;
+    min-height: 100%;
     font-weight: 400;
     box-sizing: border-box;
   }
 `;
 
 const Layout: React.FC = ({ children }) => (
-  <>
+  <ThemeProvider theme={lightTheme}>
     <GlobalStyles />
     <main>{children}</main>
-  </>
+  </ThemeProvider>
 );
 
 Layout.propTypes = {
