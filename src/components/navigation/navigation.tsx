0c48a4964345db07e1be3e1f07ddb39b50f53e52
SKP <skp.frl@gmail.com>
2020-05-28 12:31:00 +0530

    feat(components): finish styling navigation component

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
 import { useTransition } from "react-spring";
-import hamburgerSvg from "@/images/icon-hamburger.svg";
-import { Wrapper, MobileNav, Hamburger } from "@/components/navigation/styles";
+import hamburgerSvg from "@/assets/icon-hamburger.svg";
+import logo from "@/assets/logo-bookmark-white.svg";
+import closeSvg from "@/assets/icon-close.svg";
+import facebookIcon from "@/assets/icon-facebook.svg";
+import twitterIcon from "@/assets/icon-twitter.svg";
+import {
+  Wrapper,
+  MobileNav,
+  Hamburger,
+  MobileLogo,
+  MainContainer,
+  DesktopNavLinks,
+  Top,
+  CloseIcon,
+  NavLinks,
+  NavItem,
+  Bottom,
+  Social,
+  DesktopNav,
+  DesktopNavItem,
+  DesktopLoginBtn,
+} from "@/components/navigation/styles";
 
 const Navigation: React.FC = () => {
   // for mobile
@@ -9,10 +29,11 @@ const Navigation: React.FC = () => {
   const transitions = useTransition(show, null, {
     from: { opacity: 0, transformOrigin: "top right", transform: "scale(0)" },
     enter: { opacity: 1, transform: "scale(1)" },
-    leave: { opacity: 0, transform: "" },
+    leave: { opacity: 0 },
   });
 
   const onHamburgerClick = () => setShow(true);
+  const onCloseClick = () => setShow(false);
 
   return (
     <Wrapper>
@@ -22,7 +43,60 @@ const Navigation: React.FC = () => {
           alt="hamburger icon"
           onClick={onHamburgerClick}
         />
+
+        {transitions.map(
+          ({ item, key, props }) =>
+            item && (
+              <MainContainer key={key} style={props}>
+                <div>
+                  <Top>
+                    <MobileLogo src={logo} alt="bookmark logo" />
+                    <CloseIcon
+                      src={closeSvg}
+                      alt="close menu icon"
+                      onClick={onCloseClick}
+                    />
+                  </Top>
+
+                  <NavLinks>
+                    <NavItem>
+                      <a href="#">Features</a>
+                    </NavItem>
+                    <NavItem>
+                      <a href="#">Pricing</a>
+                    </NavItem>
+                    <NavItem>
+                      <a href="#">Contact</a>
+                    </NavItem>
+                    <NavItem>
+                      <a href="#">Login</a>
+                    </NavItem>
+                  </NavLinks>
+                </div>
+
+                <Bottom>
+                  <Social src={facebookIcon} alt="facebook logo" />
+                  <Social src={twitterIcon} alt="twitter logo" />
+                </Bottom>
+              </MainContainer>
+            )
+        )}
       </MobileNav>
+
+      <DesktopNav>
+        <DesktopNavLinks>
+          <DesktopNavItem>
+            <a href="#">Features</a>
+          </DesktopNavItem>
+          <DesktopNavItem>
+            <a href="#">Pricing</a>
+          </DesktopNavItem>
+          <DesktopNavItem>
+            <a href="#">Contact</a>
+          </DesktopNavItem>
+          <DesktopLoginBtn>Login</DesktopLoginBtn>
+        </DesktopNavLinks>
+      </DesktopNav>
     </Wrapper>
   );
 };
--- src/components/navigation/styles.ts
+++ src/components/navigation/styles.ts
@@ -1,10 +1,155 @@
 import styled from "styled-components";
+import { animated } from "react-spring";
+import device from "@/utils/media";
 
 export const Wrapper = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   text-transform: uppercase;
 `;
 
-export const MobileNav = styled.nav``;
+export const MobileNav = styled.nav`
+  color: ${props => props.theme.colors.white};
 
-export const Hamburger = styled.img``;
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
+export const DesktopNav = styled.nav`
+  display: none;
+  width: 30rem;
+  font-size: 1.1rem;
+
+  @media ${device.tablet} {
+    display: block;
+  }
+`;
+
+export const Hamburger = styled.img`
+  cursor: pointer;
+`;
+
+export const MobileLogo = styled.img`
+  padding-right: 1rem;
+`;
+
+// the overlay container shown when user clicks on menu icon
+export const MainContainer = styled(animated.div)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2.7rem 1.8rem;
+  background-color: ${props => props.theme.colors.darkBlue};
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 2;
+  opacity: 0.9;
+`;
+
+// close icon and logo
+export const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2.7rem;
+`;
+
+export const CloseIcon = styled.img`
+  cursor: pointer;
+  fill: ${props => props.theme.colors.white};
+  transition: transform 0.15s ease-in-out;
+
+  :hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const NavLinks = styled.li`
+  list-style: none;
+  border-top: 1px solid rgba(255, 255, 255, 0.3);
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export const DesktopNavLinks = styled(NavLinks)`
+  border-top: none;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const DesktopLoginBtn = styled.button`
+  cursor: pointer;
+  outline: 0;
+  border: 0;
+  padding: 0.6rem 1.5rem;
+  font-size: inherit;
+  font-family: inherit;
+  color: ${props => props.theme.colors.white};
+  transition: all 0.2s ease-in-out;
+  border-radius: 0.3rem;
+  background-color: ${props => props.theme.colors.softRed};
+  box-shadow: 0rem 0.3rem 0.6rem
+    rgba(${props => props.theme.colors.rgbSoftRed}, 0.25);
+  text-transform: uppercase;
+
+  :hover {
+    transform: translateY(-0.1rem);
+    box-shadow: 0rem 0.4rem 1rem
+      rgba(${props => props.theme.colors.rgbSoftRed}, 0.25);
+  }
+`;
+
+export const NavItem = styled.ul`
+  width: 100%;
+  height: 4rem;
+  letter-spacing: 0.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  transition: color 0.2s ease-in-out;
+
+  :hover {
+    color: ${props => props.theme.colors.softRed};
+  }
+`;
+
+export const DesktopNavItem = styled.ul`
+  color: ${props => props.theme.colors.grayishBlue};
+
+  ::after {
+    content: "";
+    display: block;
+    width: 100%;
+    margin-top: 0.1rem;
+    height: 0.2rem;
+    background-color: ${props => props.theme.colors.softRed};
+    transform: scaleX(0);
+    transition: transform 0.2s ease-in-out;
+  }
+
+  :hover ::after {
+    transform: scaleX(1);
+  }
+`;
+
+// contains social media links
+export const Bottom = styled.div`
+  width: 30%;
+  margin: 0 auto; /* center it */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Social = styled.img`
+  cursor: pointer;
+`;
