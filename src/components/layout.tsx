18dd124f183bfa67b2017a4b80e8cb47029125f7
SKP <skp.frl@gmail.com>
2020-05-29 11:45:24 +0530

    feat(layout): make layout global styles more responsive

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import "typeface-rubik";
 import lightTheme from "@/themes/light-theme";
+import device from "@/utils/media";
 
 const GlobalStyles = createGlobalStyle`
   *,
@@ -11,23 +12,44 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: inherit;
+    -webkit-tap-highlight-color: transparent;
   }
 
   body {
-    font-size: 10px;
-    font-family: "Rubik", sans-serif;
     color: ${props => props.theme.colors.darkBlue};
     position: relative;
     min-height: 100%;
     font-weight: 400;
     box-sizing: border-box;
+    overflow-x: hidden;
+    padding: 0rem 2.3rem;
+    padding-bottom: 3rem;
+
+    @media ${device.tablet} {
+      padding: 0rem 3rem;
+    }
+
+    @media ${device.laptop} {
+      padding: 0rem 5rem;
+    }
+  }
+
+  html, body {
+    font-size: 10px;
+    font-family: "Rubik", sans-serif;
+
+    @media ${device.laptop} {
+      font-size: 11px;
+    }
   }
 `;
 
 const Layout: React.FC = ({ children }) => (
   <ThemeProvider theme={lightTheme}>
-    <GlobalStyles />
-    <main>{children}</main>
+    <main>
+      <GlobalStyles />
+      {children}
+    </main>
   </ThemeProvider>
 );
 
