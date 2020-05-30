e5447c44daef9b80688fe8694bebd4bd7721c37d
SKP <skp.frl@gmail.com>
2020-05-30 15:13:04 +0530

    feat: implement Features and DownloadSection component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,51 @@
 import React from "react";
-import { useStaticQuery, graphql, PageProps } from "gatsby";
+import { graphql, PageProps } from "gatsby";
 import styled from "styled-components";
 import Layout from "@/components/layout";
 import Header from "@/components/header/header.tsx";
 import Description from "@/components/description/description.tsx";
+import Features from "@/components/features/features.tsx";
+import DownloadSection from "@/components/download-section/download-section.tsx";
 import SEO from "@/components/seo";
 
 const GridContainer = styled.div`
   display: grid;
   grid-template-rows: repeat(5, min-content);
-  row-gap: 5rem;
+  row-gap: 12rem;
 
   p {
     line-height: 1.45;
   }
 `;
 
-const IndexPage: React.FC<PageProps> = () => {
-  const { site } = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
+type QueryData = {
+  site: {
+    siteMetadata: {
+      title: string;
+    };
+  };
+};
+
+const IndexPage: React.FC<PageProps<QueryData>> = ({ data }) => (
+  <Layout>
+    <SEO title={data.site.siteMetadata.title} />
+    <Header />
+    <GridContainer>
+      <Description />
+      <Features />
+      <DownloadSection />
+    </GridContainer>
+  </Layout>
+);
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
       }
     }
-  `);
-
-  return (
-    <Layout>
-      <SEO title={site.siteMetadata.title} />
-      <Header />
-      <GridContainer>
-        <Description />
-      </GridContainer>
-    </Layout>
-  );
-};
+  }
+`;
 
 export default IndexPage;
